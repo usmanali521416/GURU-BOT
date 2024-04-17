cfc94f3d56357d8ee2e4bf56239e44239af62cec
usmanali521416 <154719556+usmanali521416@users.noreply.github.com>
2024-04-17 13:32:30 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923216987454', 'usman', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
